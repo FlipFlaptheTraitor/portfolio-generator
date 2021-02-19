6989a35cf654be4d22d1536f1930bea32d7caff1
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-02-18 22:24:47 -0500

    map and slice

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-//const generatePage = require('./src/page-template');
-
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
-  //if (err) throw err;
-
- // console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 inquirer
 const promptUser = () => {
   return inquirer.prompt([
@@ -138,4 +130,10 @@ promptUser()
   .then(promptProject)
   .then(portfolioData => {
     console.log(portfolioData);
+  
+     const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
   });
